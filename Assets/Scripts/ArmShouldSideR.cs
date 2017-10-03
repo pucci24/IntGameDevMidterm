5c0044281d31545d005e184bb8c6aa46e828988d
Keanan Pucci <kcp320@nyu.edu>
2017-10-03 03:07:05 -0400

    First Prototype
    Lots of work to do but plenty learned for moving forward

---
+++ Assets/Scripts/ArmShouldSideR.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArmShouldSideR : MonoBehaviour {
+
+	Rigidbody myRigidbody;
+	Vector3 inputVector; // gets Input from Update and sends it to physics
+
+	// Use this for initialization
+	void Start () {
+		myRigidbody = GetComponent<Rigidbody>(); // assign RB reference at Start
+	}
+	
+	// Update is called once per frame
+	void Update () {
+			if (Input.GetKey (KeyCode.P)) {
+				inputVector = new Vector3 (0f, 0f, 100f);
+			} else {
+				inputVector = new Vector3 (0f, 0f, -20f);
+			}
+		
+	}
+
+	// FixedUpdate runs at a "Fixed Timestep", which is when physics run
+	void FixedUpdate () {
+		myRigidbody.AddRelativeForce(inputVector);	
+
+		Debug.Log("my velocity: " + myRigidbody.velocity.ToString() );
+		Debug.Log( "my speed: " + myRigidbody.velocity.magnitude.ToString() );
+	}
+
+}
